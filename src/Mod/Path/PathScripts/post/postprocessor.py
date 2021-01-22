ecef96175d55404b445dc258703f9343b0d6b4cf
sliptonic <shopinthewoods@gmail.com>
2021-01-22 10:57:16 -0600

    implement base and rework linuxcnc

---
+++ src/Mod/Path/PathScripts/post/postprocessor.py
@@ -0,0 +1,382 @@
+# ***************************************************************************
+# *   Copyright (c) 2014 sliptonic <shopinthewoods@gmail.com>               *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from __future__ import print_function
+import FreeCAD
+from FreeCAD import Units
+import Path
+import argparse
+import datetime
+from PathScripts import PostUtils
+import shlex
+
+
+parser = argparse.ArgumentParser(prog='linuxcnc', add_help=False)
+
+
+class ObjectPost(object):
+    '''
+    Base class for postprocessor objects.
+
+    use this class as the base class for new postprocessors.  It provides
+    properties and functionality for the standard posts.
+    '''
+    def __init__(self, name):
+        self._name = name
+
+        # Output options
+        self._output_comments = True
+        self._output_header = True
+        self._output_line_numbers = False
+        self._linenr = 0  # line number starting value
+        self._show_editor = True
+        self._precision = 3
+        self._command_space = " "
+
+        # Options to reduce gcode size
+        self._modal = False  # if true commands are suppressed if the same as previous line.
+        self._output_doubles = True  # if false duplicate axis values are suppressed if the same as previous line.
+
+        # Tool Change options
+        self._use_tlo = True # if true G43 will be output following tool changes
+        self._tool_change = ''''''
+
+        # Units
+        self._units = "G21"  # G21 for metric, G20 for us standard
+        self._unit_speed_format = 'mm/min'
+        self._unit_format = 'mm'
+
+        # These globals will be reflected in the Machine configuration of the project
+        # Pre operation text will be inserted before every operation
+        self._pre_operation = ''''''
+
+        # Post operation text will be inserted after every operation
+        self._post_operation = ''''''
+
+        # Tool Change commands will be inserted before a tool change
+
+
+        # Job Preamble text will appear at the beginning of the GCODE output file.
+        self._preamble = '''G17 G54 G40 G49 G80 G90'''
+
+        # Job Postamble text will appear following the last operation.
+        self._postamble = '''M05
+G17 G54 G90 G80 G40\n
+M2\n'''
+
+    def getArgs(self):
+        '''
+        creates an arparser and adds supported arguments.
+        Can be extended
+        '''
+        parser = argparse.ArgumentParser(prog=self._name, add_help=False)
+
+        parser.add_argument('--no-header', action='store_true', help='suppress header output')
+        parser.add_argument('--no-comments', action='store_true', help='suppress comment output')
+        parser.add_argument('--line-numbers', action='store_true', help='prefix with line numbers')
+        parser.add_argument('--no-show-editor', action='store_true', help='don\'t pop up editor before writing output')
+        parser.add_argument('--precision', default='3', help='number of digits of precision, default=3')
+        parser.add_argument('--preamble', help='set commands to be issued before the first command, default="G17\nG90"')
+        parser.add_argument('--postamble', help='set commands to be issued after the last command, default="M05\nG17 G90\nM2"')
+        parser.add_argument('--inches', action='store_true', help='Convert output for US imperial mode (G20)')
+        parser.add_argument('--modal', action='store_true', help='Output the Same G-command Name USE NonModal Mode')
+        parser.add_argument('--axis-modal', action='store_true', help='Output the Same Axis Value Mode')
+        parser.add_argument('--no-tlo', action='store_true', help='suppress tool length offset (G43) following tool changes')
+        self.TOOLTIP_ARGS = parser.format_help()
+
+        return parser
+
+    def processArguments(self, argstring):
+        try:
+            args = parser.parse_args(shlex.split(argstring))
+            if args.no_header:
+                self._output_header = False
+            if args.no_comments:
+                self._output_comments = False
+            if args.line_numbers:
+                self._output_line_numbers = True
+            if args.no_show_editor:
+                self._show_editor = False
+            print("Show editor = %d" % self._show_editor)
+            self._precision = args.precision
+            if args.preamble is not None:
+                self._preamble = args.preamble
+            if args.postamble is not None:
+                self._postamble = args.postamble
+            if args.inches:
+                self._units = 'G20'
+                self._unit_speed_format = 'in/min'
+                self._unit_format = 'in'
+                self._precision = 4
+            if args.modal:
+                self._modal = True
+            if args.no_tlo:
+                self._use_tlo = False
+            if args.axis_modal:
+                self._output_doubles = False
+
+        except Exception as e: # pylint: disable=broad-except
+            print(e)
+            return False
+
+        return True
+
+
+    def linenumber(self):
+        '''
+        calculates the line number
+
+        Can be safely overriden
+        '''
+        if self._output_line_numbers:
+            self._linenr += 10
+            return "N {} ".format(self._linenr)
+        else:
+            return ""
+
+
+    def parse(self, pathobj):
+        '''
+        Parses a single path object to output gcode.
+
+        Can be safely overriden
+        '''
+
+        out = ""
+        lastcommand = None
+        precision_string = '.' + str(self._precision) + 'f'
+        currLocation = {}  # keep track for no doubles
+
+        # the order of parameters
+        # linuxcnc doesn't want K properties on XY plane  Arcs need work.
+        params = ['X', 'Y', 'Z', 'A', 'B', 'C', 'I', 'J', 'F', 'S', 'T', 'Q', 'R', 'L', 'H', 'D', 'P']
+        firstmove = Path.Command("G0", {"X": -1, "Y": -1, "Z": -1, "F": 0.0})
+        currLocation.update(firstmove.Parameters)  # set First location Parameters
+
+        if hasattr(pathobj, "Group"):  # We have a compound or project.
+            # if OUTPUT_COMMENTS:
+            #     out += linenumber() + "(compound: " + pathobj.Label + ")\n"
+            for p in pathobj.Group:
+                out += self.parse(p)
+            return out
+        else:  # parsing simple path
+
+            # groups might contain non-path things like stock.
+            if not hasattr(pathobj, "Path"):
+                return out
+
+            for c in pathobj.Path.Commands:
+
+                outstring = []
+                command = c.Name
+                outstring.append(command)
+
+                # if modal: suppress the command if it is the same as the last one
+                if self._modal is True:
+                    if command == lastcommand:
+                        outstring.pop(0)
+
+                if c.Name[0] == '(' and not self._output_comments: # command is a comment
+                    continue
+
+                # Now add the remaining parameters in order
+                for param in params:
+                    if param in c.Parameters:
+                        if param == 'F' and (currLocation[param] != c.Parameters[param] or self._output_doubles):
+                            if c.Name not in ["G0", "G00"]:  # linuxcnc doesn't use rapid speeds
+                                speed = Units.Quantity(c.Parameters['F'], FreeCAD.Units.Velocity)
+                                if speed.getValueAs(self._unit_speed_format) > 0.0:
+                                    outstring.append(param + format(float(speed.getValueAs(self._unit_speed_format)), precision_string))
+                            else:
+                                continue
+                        elif param == 'T':
+                            outstring.append(param + str(int(c.Parameters['T'])))
+                        elif param == 'H':
+                            outstring.append(param + str(int(c.Parameters['H'])))
+                        elif param == 'D':
+                            outstring.append(param + str(int(c.Parameters['D'])))
+                        elif param == 'S':
+                            outstring.append(param + str(int(c.Parameters['S'])))
+                        else:
+                            if (not self._output_doubles) and (param in currLocation) and (currLocation[param] == c.Parameters[param]):
+                                continue
+                            else:
+                                pos = Units.Quantity(c.Parameters[param], FreeCAD.Units.Length)
+                                outstring.append(
+                                    param + format(float(pos.getValueAs(self._unit_format)), precision_string))
+
+                # store the latest command
+                lastcommand = command
+                currLocation.update(c.Parameters)
+
+                # Check for Tool Change:
+                if command == 'M6':
+                    # stop the spindle
+                    out += self.linenumber() + "M5\n"
+                    for line in self._tool_change.splitlines(True):
+                        out += self.linenumber() + line
+
+                    # add height offset
+                    if self._use_tlo:
+                        tool_height = '\nG43 H' + str(int(c.Parameters['T']))
+                        outstring.append(tool_height)
+
+                if command == "message":
+                    if self._output_comments is False:
+                        out = []
+                    else:
+                        outstring.pop(0)  # remove the command
+
+                # prepend a line number and append a newline
+                if len(outstring) >= 1:
+                    if self._output_line_numbers:
+                        outstring.insert(0, (self.linenumber()))
+
+                    # append the line to the final output
+                    for w in outstring:
+                        out += w + self._command_space
+                    # Note: Do *not* strip `out`, since that forces the allocation
+                    # of a contiguous string & thus quadratic complexity.
+                    out += "\n"
+
+            return out
+
+
+    def export(self, objectslist):
+        '''
+        Processes an entire job and generate gcode.
+
+        Can be safely overriden
+        '''
+
+        #reset line number to 0
+        self._linenr = 0
+
+        for obj in objectslist:
+            if not hasattr(obj, "Path"):
+                print("the object {} is not a post-processable.".format(obj.Name))
+                return None
+
+        print("postprocessing...")
+        gcode = ""
+
+        # write header
+        if self._output_header:
+            now = datetime.datetime.now()
+            gcode += self.linenumber() + "(Exported by FreeCAD)\n"
+            gcode += self.linenumber() + "(Post Processor: {}\n".format(self._name)
+            gcode += self.linenumber() + "(Output Time: {})\n".format(now)
+
+        # Write the preamble
+        if self._output_comments:
+            gcode += self.linenumber() + "(begin preamble)\n"
+        for line in self._preamble.splitlines(False):
+            gcode += self.linenumber() + line + "\n"
+        gcode += self.linenumber() + self._units + "\n"
+
+        for obj in objectslist:
+
+            # Skip inactive operations
+            if hasattr(obj, 'Active'):
+                if not obj.Active:
+                    continue
+            if hasattr(obj, 'Base') and hasattr(obj.Base, 'Active'):
+                if not obj.Base.Active:
+                    continue
+
+            # do the pre_op
+            if self._output_comments:
+                gcode += self.linenumber() + "(begin operation: %s)\n" % obj.Label
+                gcode += self.linenumber() + "(speed format: {})\n".format(self._unit_speed_format)
+            for line in self._pre_operation.splitlines(True):
+                gcode += self.linenumber() + line
+
+            # get coolant mode
+            coolantMode = 'None'
+            if hasattr(obj, "CoolantMode") or hasattr(obj, 'Base') and  hasattr(obj.Base, "CoolantMode"):
+                if hasattr(obj, "CoolantMode"):
+                    coolantMode = obj.CoolantMode
+                else:
+                    coolantMode = obj.Base.CoolantMode
+
+            # turn coolant on if required
+            if self._output_comments:
+                if not coolantMode == 'None':
+                    gcode += self.linenumber() + '(Coolant On:' + coolantMode + ')\n'
+            if coolantMode == 'Flood':
+                gcode  += self.linenumber() + 'M8' + '\n'
+            if coolantMode == 'Mist':
+                gcode += self.linenumber() + 'M7' + '\n'
+
+            # process the operation gcode
+            gcode += self.parse(obj)
+
+            # do the post_op
+            if self._output_comments:
+                gcode += self.linenumber() + "(finish operation: %s)\n" % obj.Label
+            for line in self._post_operation.splitlines(True):
+                gcode += self.linenumber() + line
+
+            # turn coolant off if required
+            if not coolantMode == 'None':
+                if self._output_comments:
+                    gcode += self.linenumber() + '(Coolant Off:' + coolantMode + ')\n'
+                gcode  += self.linenumber() +'M9' + '\n'
+
+        # do the post_amble
+        if self._output_comments:
+            gcode += "(begin postamble)\n"
+        for line in self._postamble.splitlines(True):
+            gcode += self.linenumber() + line
+
+        if FreeCAD.GuiUp and self._show_editor:
+            final = gcode
+            if len(gcode) > 100000:
+                print("Skipping editor since output is greater than 100kb")
+            else:
+                dia = PostUtils.GCodeEditorDialog()
+                dia.editor.setText(gcode)
+                result = dia.exec_()
+                if result:
+                    final = dia.editor.toPlainText()
+        else:
+            final = gcode
+
+        print("Gcode Generated")
+        return final
+
+    def writeFile(self, finalgcode, filename):
+        '''
+        Writes gcode to target file.
+
+        Can be safely overriden
+        '''
+        if not filename == '-':
+            with open(filename, 'w') as gfile:
+                gfile.write(finalgcode)
+
+            print("File Written to {}".format(filename))
+            return True
+        return False
+
+
