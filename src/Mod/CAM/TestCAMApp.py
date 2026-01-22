f69bb53b2274f9af4973f1a4552485a71bacc076
sliptonic <shopinthewoods@gmail.com>
2026-01-22 15:31:35 -0600

    update machines

--- src/Mod/CAM/TestCAMApp.py
+++ src/Mod/CAM/TestCAMApp.py
@@ -119,7 +119,7 @@ from CAMTests.TestFanucPost import TestFanucPost
 # from CAMTests.TestGrblPost import TestGrblPost
 # from CAMTests.TestMassoG3Post import TestMassoG3Post
 # from CAMTests.TestCentroidPost import TestCentroidPost
-from CAMTests.TestMach3Mach4Post import TestMach3Mach4Post
+# from CAMTests.TestMach3Mach4Post import TestMach3Mach4Post
 from CAMTests.TestTestPost import TestTestPost
 from CAMTests.TestPostGCodes import TestPostGCodes
 from CAMTests.TestPostMCodes import TestPostMCodes
